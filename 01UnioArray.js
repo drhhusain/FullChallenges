9e10dd40b4c66cf6a35057cce25cf2c1bd80703d
Hamid Hussain <hamidhusain851@gmail.com>
2022-04-18 14:01:56 +0530

    Implemented For Loop

--- 01UnioArray.js
+++ 01UnioArray.js
@@ -7,18 +7,38 @@
 // [1,2,3,4,5,6]
 
 
+// Sol 1: We can change in either one of the existing array (I chose arrTwo)
+// Sol 2: We can make another array and fill it up by unique numbers from both array
 
 const arrOne = [1,2,5];
 const arrTwo = [2,3,4,6];
 
-console.log("Array 1 : " + arrOne);
-console.log("Array 2 : " + arrTwo);
+console.log("Array 1 : \n");
+console.log(arrOne);
+console.log("Array 2 : \n");
+console.log(arrTwo);
+console.log("Output : \n");
+console.log(arrTwo);
 var index = arrTwo.length;
 
-arrOne.forEach(currentElement => {
-  if (arrTwo.indexOf(currentElement) == -1 ) {
-    arrTwo[index++] = currentElement;
-  }
-});
+// using for lopp
 
-console.log("Output : " + arrTwo);
+for (let i = 0; i < arrOne.length; i++) {
+  if (arrTwo.indexOf(arrOne[i]) == -1) {
+    arrTwo[index++] = arrOne[i];
+  }
+}
+
+console.log("Output : \n");
+console.log(arrTwo);
+
+// using forEach loop
+
+// arrOne.forEach(currentElement => {
+//   if (arrTwo.indexOf(currentElement) == -1 ) {
+//     arrTwo[index++] = currentElement;
+//   }
+// });
+
+// console.log("Output : \n");
+// console.log(arrTwo);
